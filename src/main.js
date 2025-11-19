c2f9235d0049ed0eb4a6a7a956a1c7cb0db3f926
suarezmanuel <manuelsuarez244@gmail.com>
2025-11-19 19:21:32 -0300

    removed files

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { testQuery, testCreateBuyEntity } from './tests.js';
-
-localStorage.debug = 'arkiv:*'
-// Function to be called from HTML button
-// window.connectAndWrite = testCreateBuyEntity;
-window.connectAndWrite = testQuery;
